bc71faba6d43f0cb11642d2bfdd6af238083b514
Boaz Crezee <bjcrezee@gmail.com>
2025-02-07 21:36:33 +0100

    Fixed the memory bank data generator and added displaying methods

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 import ExtendedMath from "./extended_math.js";
 import MemoryBankGenerator from "./memory_bank_generator.js";
 
-const generator = new MemoryBankGenerator(0);
+const generator = new MemoryBankGenerator(1);
 
-console.log(generator.generate((bankIndex, bankPosition) => {
+generator.generate((bankIndex, bankPosition) => {
     const x = ExtendedMath.intDivMod(bankIndex, 1, 16);
     const y = ExtendedMath.intDivMod(bankIndex, 16, 16);
     return x + y;
-}));
+});
+
+const paragraphElement = document.querySelector('p');
+if (paragraphElement) {
+    generator.write(paragraphElement);
+}
+
+console.log(generator.getFormattedData());
 
 // console.log(ExtendedMath.getHexDigit(254, 0));
 // console.log(ExtendedMath.getHexDigit(254, 1));
